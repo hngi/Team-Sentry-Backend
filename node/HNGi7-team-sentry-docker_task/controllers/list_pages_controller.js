9d7610b3b1740739f14a2a0b56cb4314d2110a0d
Micah Elijah <melijah200@gmail.com>
2020-06-11 15:22:36 +0100

    initial commit

---
+++ node/HNGi7-team-sentry-docker_task/controllers/list_pages_controller.js
@@ -0,0 +1,26 @@
+const router = require("express").Router(),
+            Data = require("../Resources/database/database"),
+            Config = require("../config/config"),
+            mongoose = require("mongoose"),
+            config = require("../config/config");
+
+// request = require("request"),
+// mongoose = require("mongoose");
+router.get("/list_pages", (req, res) => {
+    if(mongoose.connection.readyState == 1){
+        // const url = req.body.url;
+        Data.find({}, (err, pages) => {
+            if (err)
+                res.send(err);
+            res.json(pages)
+        })
+    }
+    else{
+        res.status(503).json({
+            status: "failed",
+            message: "No database connection established"
+        })
+    }
+})
+
+module.exports = router;
